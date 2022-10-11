adb14641434e9dfd4d37eca4efba82358673fa14
goriio <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2022-10-11 15:55:26 +0800

    add button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import { Input } from '@/components';
-
 export default function Home() {
   return (
-    <div className="grid place-items-center max-w-xl mx-auto p-4 h-screen"></div>
+    <div className="grid place-items-center max-w-lg mx-auto p-4 h-screen"></div>
   );
 }
