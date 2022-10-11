207f9151ac6bcf3b95ec6df8fc2eb69826ee0fab
goriio <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2022-10-11 16:19:19 +0800

    add form component for link shortener

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import { LinkShortenerForm } from '@/components';
+
 export default function Home() {
   return (
-    <div className="grid place-items-center max-w-lg mx-auto p-4 h-screen"></div>
+    <div className="grid place-items-center max-w-lg mx-auto p-4 h-screen">
+      <LinkShortenerForm />
+    </div>
   );
 }
