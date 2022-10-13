d83540d5b24c056f71bb3b327c7194f4ceb24f94
goriio <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2022-10-13 11:49:20 +0800

    add metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
+
+const app = {
+  title: 'iksi | url shortener',
+  description: '🔗 Free custom url shortener',
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="grid place-items-center max-w-lg mx-auto p-4 h-screen">
+      <Head>
+        <title>{app.title}</title>
+        <meta name="description" content={app.description} />
+
+        <meta property="og:title" content={app.title} />
+        <meta property="og:description" content={app.description} />
+        <meta property="og:image" content="/og-image.png" />
+
+        <link rel="icon" type="image/svg" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
       <Toaster
         position="bottom-right"
