19173a3964918e85d87b9114e26c7b35ac3c6f97
goriio <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2022-10-24 11:27:12 +0800

    add api documentation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import { LinkShortenerForm } from '@/components';
 
 export default function Home() {
-  return <LinkShortenerForm />;
+  return (
+    <div className="grid place-items-center max-w-lg mx-auto p-4 h-screen">
+      <LinkShortenerForm />
+    </div>
+  );
 }
